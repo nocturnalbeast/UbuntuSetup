89f59b0b5a03a81975e9960a810c8188af07c98a
nocturnalbeast <betrant1996@gmail.com>
2018-08-09 16:41:04 +0530

    Fix some incorrect menu entries, implemented more stuff and added README

---
+++ README.md
@@ -0,0 +1,24 @@
+# UbuntuSetup
+
+This repository contains a script that I hacked together to ease the PITA that was the installation of Ubuntu 18.04 LTS. I've suffered enough, let the others suffer less.
+
+If someone wants to add more to this script or change something, do let me know! Feature requests are welcome (because I'm running out of ideas on what to implement). Or you can contribute by forking the repository and adding your code.
+
+### Usage
+
+Clone or download the repository, then run these commands to use the script.
+The script's got a menu, so using it shouldn't be too hard.
+
+```
+cd UbuntuSetup
+chmod +x UbuntuSetup.sh
+./UbuntuSetup.sh
+```
+
+## Authors
+
+* **nocturnalbeast** - [nocturnalbeast](https://github.com/nocturnalbeast)
+
+## License
+
+Seriously? You need a license for a meagre script?
--- UbuntuSetup.sh
+++ UbuntuSetup.sh
@@ -15,175 +15,251 @@ cyan='\033[0;36m'
 white='\033[0;37m'
 
 echo -e "${cyan}UbuntuSetup${no_color} - Setting up ${red}Ubuntu${no_color} for lazy people!"
-
-# Write out the menu options...
 echo "Let's get started! What do ya want to do?"
-echo "Select an option:"
-echo ""
-echo -e " ${cyan}1${no_color}: Setup Proxy for apt"
-echo -e " ${cyan}2${no_color}: Update packages"
-echo -e " ${cyan}3${no_color}: Install another desktop environment"
-echo -e " ${cyan}4${no_color}: Uninstall default desktop environment"
-echo -e " ${cyan}5${no_color}: Install display and terminal fonts"
-echo -e " ${cyan}6${no_color}: Install a IDE or advanced text editor"
-echo -e " ${cyan}7${no_color}: Beautify your system"
-echo -e " ${cyan}8${no_color}: Set root password"
-echo -e " ${cyan}9${no_color}: Remove snap applications and snapd"
-echo -e " ${cyan}10${no_color}: Remove Canonical's usage tracking"
-echo -e " ${cyan}11${no_color}: Clean up packages"
-echo -e " ${cyan}12${no_color}: Install preferred applications"
 
-echo -e " ${cyan}15${no_color}: Exit"
+while :
+do
+	# Write out the menu options...
+	echo "Select an option:"
+	echo ""
+	echo -e " ${cyan}1${no_color}: Setup Proxy for apt"
+	echo -e " ${cyan}2${no_color}: Update packages"
+	echo -e " ${cyan}3${no_color}: Install another desktop environment"
+	echo -e " ${cyan}4${no_color}: Uninstall default desktop environment"
+	echo -e " ${cyan}5${no_color}: Install display and terminal fonts"
+	echo -e " ${cyan}6${no_color}: Install a IDE or advanced text editor"
+	echo -e " ${cyan}7${no_color}: Beautify your system"
+	echo -e " ${cyan}8${no_color}: Set root password"
+	echo -e " ${cyan}9${no_color}: Remove snap applications and snapd"
+	echo -e " ${cyan}10${no_color}: Remove bloat and tracking"
+	echo -e " ${cyan}11${no_color}: Clean up packages"
+	echo -e " ${cyan}12${no_color}: Install preferred applications"
+	echo -e " ${cyan}13${no_color}: Install restricted extras"
+	echo -e " ${cyan}14${no_color}: Set up IDE extensions (for VS Code only)"
 
-#Do eeet!
-read fch
-case $fch in
-	1) 
-		sudo touch /etc/apt/apt.conf.d/02proxy
-		sudo echo "proxylineone" >> /etc/apt/apt.conf.d/02proxy
-		sudo echo "proxylinetwo" >> /etc/apt/apt.conf.d/02proxy
-		echo -e "${green}Proxy set up!"
-		;;
-	2) 
-		sudo apt-get update
-		sudo apt-get -y upgrade
-		echo -e "${green}System's spankin new now!"
-		;;
-	3) 
-		echo "Pick your poison:"
-		echo -e " ${cyan}a${no_color}: LXDE"
-		echo -e " ${cyan}b${no_color}: XFCE"
-		echo -e " ${cyan}c${no_color}: Budgie"
-		echo -e " ${cyan}d${no_color}: KDE"
-		echo -e " ${cyan}e${no_color}: Cinnamon"
-		echo -e " ${cyan}f${no_color}: Mate"
-		echo -e " ${cyan}g${no_color}: Unity (don't say I didn't warn ya.)"
+	echo -e " ${cyan}15${no_color}: Exit"
 
-		read dech
-		case $dech in
-    		a) 
-				sudo apt install tasksel
-				sudo tasksel install lubuntu-desktop
-				;;
-    		b) 
-				sudo apt install tasksel
-				sudo tasksel install xubuntu-desktop
-				;;
-    		c) 
-				sudo apt install ubuntu-budgie-desktop
-				;;
-			d) 
-				sudo apt install tasksel
-				sudo tasksel install kubuntu-desktop
-				;;
-			e) 
-				sudo apt install cinnamon-desktop-environment lightdm
-				sudo dpkg-reconfigure
-				;;
-			f) 
-				sudo apt install ubuntu-unity-desktop
-				;;
-    		*) 
-				echo -e "${red}Sorry kiddo, that isn't a valid DE choice."
-				;;
-		esac
-		echo -e "${green}Enjoy your new DE!"
-		;;
-	4) 
-		read -p "This will remove the default GNOME desktop and GDM login manager. Sure you want to continue? ${yellow}(y/n)${no_color}:" yn
-    	case $yn in
-        	[Yy]* ) 
-				sudo apt-get remove -y --auto-remove ubuntu-gnome-desktop
-				sudo apt-get remove -y gnome-shell
-				sudo apt-get purge --auto-remove ubuntu-gnome-desktop
-				sudo apt-get autoremove
-				sudo dpkg-reconfigure gdm
-				sudo apt-get remove gdm
-				echo -e "${green}Buhbye GNOME!"
-				;;
-        	[Nn]* ) 
-				continue
-				;;
-        	* ) 
-				echo -e "${red}It's invalid kiddo! Try again.";;
-    	esac
-		;;
-	5) 
-		iosevka_loc="https://github.com/be5invis/Iosevka/releases/download/v2.0.0/02-iosevka-term-2.0.0.zip"
-		hack_loc="https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip"
-		input_loc="http://input.fontbureau.com/build/?fontSelection=whole&a=0&g=0&i=0&l=0&zero=0&asterisk=0&braces=0&preset=default&line-height=1.2&accept=I+do&email="
-		overpass_loc="https://github.com/RedHatBrand/Overpass/releases/download/3.0.2/overpass-desktop-fonts.zip"
-		google_loc="https://fonts.google.com/download?family=Dosis|Work%20Sans|Nanum%20Gothic|Fira%20Sans|Nunito|Noto%20Serif|PT%20Sans%20Narrow|Arimo|Muli|Roboto%20Mono|PT%20Serif|Titillium%20Web|Playfair%20Display|Lora|Noto%20Sans|Ubuntu|Open%20Sans%20Condensed|Merriweather|PT%20Sans|Roboto%20Slab|Raleway|Slabo%2027px|Source%20Sans%20Pro|Montserrat|Roboto%20Condensed|Oswald|Lato|Open%20Sans|Roboto"
+	#Do eeet!
+	read fch
+	case $fch in
+		1) 
+			read -p "Do you want to enable or disable the proxy? (y to enable, n to disable)" yn
+			case $yn in
+				[Yy]* )
+					sudo touch /etc/apt/apt.conf.d/02proxy
+					sudo echo "proxylineone" >> /etc/apt/apt.conf.d/02proxy
+					sudo echo "proxylinetwo" >> /etc/apt/apt.conf.d/02proxy
+					echo -e "${green}Proxy set up!"
+					;;
+				[Nn]* ) 
+					sudo rm /etc/apt/apt.conf.d/02proxy
+					;;
+				* ) 
+					echo -e "${red}It's invalid kiddo! Try again."
+					;;
+			esac
+			;;
+		2) 
+			sudo apt-get update
+			sudo apt-get -y upgrade
+			echo -e "${green}System's spankin new now!"
+			;;
+		3) 
+			echo "Pick your poison:"
+			echo -e " ${cyan}a${no_color}: LXDE"
+			echo -e " ${cyan}b${no_color}: XFCE"
+			echo -e " ${cyan}c${no_color}: Budgie"
+			echo -e " ${cyan}d${no_color}: KDE"
+			echo -e " ${cyan}e${no_color}: Cinnamon"
+			echo -e " ${cyan}f${no_color}: Mate"
+			echo -e " ${cyan}g${no_color}: Enlightenment"
+			echo -e " ${cyan}h${no_color}: Unity (don't say I didn't warn ya.)"
 
-		;;
-	6) 
-		echo "We have these in stock. Whaddaya need?"
-		echo " a: VS Code"
-		echo " b: Atom"
-		echo " c: Jupyter"
-		echo " d: Brackets"
-		echo " e: Eclipse"
+			read dech
+			case $dech in
+				a) 
+					sudo apt install tasksel
+					sudo apt update
+					sudo tasksel install lubuntu-desktop
+					;;
+				b) 
+					sudo apt install tasksel
+					sudo apt update
+					sudo tasksel install xubuntu-desktop
+					;;
+				c) 
+					sudo apt install -y ubuntu-budgie-desktop
+					;;
+				d) 
+					sudo apt install tasksel
+					sudo apt update
+					sudo tasksel install kubuntu-desktop
+					;;
+				e) 
+					sudo apt install -y cinnamon-desktop-environment lightdm
+					sudo dpkg-reconfigure
+					;;
+				f)
+					sudo apt install tasksel
+					sudo apt update
+					sudo tasksel install ubuntu-mate-desktop
+					;;
+				g) 
+					sudo add-apt-repository ppa:niko2040/e19
+					sudo apt update
+					sudo apt install -y enlightenment terminology
+					;;
+				h) 
+					sudo apt install -y ubuntu-unity-desktop
+					;;
+				*) 
+					echo -e "${red}Sorry kiddo, that isn't a valid DE choice."
+					;;
+			esac
+			echo -e "${green}Enjoy your new DE!"
+			;;
+		4) 
+			read -p "This will remove the default GNOME desktop and GDM login manager. Sure you want to continue? ${yellow}(y/n)${no_color}:" yn
+			case $yn in
+				[Yy]* ) 
+					sudo apt-get remove -y --auto-remove ubuntu-gnome-desktop
+					sudo apt-get remove -y gnome-shell
+					sudo apt-get purge --auto-remove ubuntu-gnome-desktop
+					sudo apt-get autoremove
+					sudo dpkg-reconfigure gdm
+					sudo apt-get remove -y gdm3
+					echo -e "${green}Buhbye GNOME!"
+					;;
+				[Nn]* ) 
+					continue
+					;;
+				* ) 
+					echo -e "${red}It's invalid kiddo! Try again."
+					;;
+			esac
+			;;
+		5) 
+			iosevka_loc="https://github.com/be5invis/Iosevka/releases/download/v2.0.0/02-iosevka-term-2.0.0.zip"
+			hack_loc="https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip"
+			input_loc="http://input.fontbureau.com/build/?fontSelection=whole&a=0&g=0&i=0&l=0&zero=0&asterisk=0&braces=0&preset=default&line-height=1.2&accept=I+do&email="
+			overpass_loc="https://github.com/RedHatBrand/Overpass/releases/download/3.0.2/overpass-desktop-fonts.zip"
+			google_loc="https://fonts.google.com/download?family=Dosis|Work%20Sans|Nanum%20Gothic|Fira%20Sans|Nunito|Noto%20Serif|PT%20Sans%20Narrow|Arimo|Muli|Roboto%20Mono|PT%20Serif|Titillium%20Web|Playfair%20Display|Lora|Noto%20Sans|Ubuntu|Open%20Sans%20Condensed|Merriweather|PT%20Sans|Roboto%20Slab|Raleway|Slabo%2027px|Source%20Sans%20Pro|Montserrat|Roboto%20Condensed|Oswald|Lato|Open%20Sans|Roboto"
+			#to be done - curl commands and install
+			;;
+		6) 
+			echo "We have these in stock. Whaddaya need?"
+			echo " ${cyan}a${no_color}: VS Code"
+			echo " ${cyan}b${no_color}: Atom"
+			echo " ${cyan}c${no_color}: Jupyter"
+			echo " ${cyan}d${no_color}: Brackets"
+			echo " ${cyan}e${no_color}: Eclipse"
+			echo " ${cyan}f${no_color}: Android Studio"
 
-		read idech
-		case $idech in
-    		a) 
-				mkdir codefolder && cd codefolder
-				curl -O -L "https://go.microsoft.com/fwlink/?LinkID=760868"
-				mv * code.deb
-				sudo dpkg -i code.deb
-				sudo apt-get install -f
-				cd ..
-				rm -rf codefolder
-				;;
-    		b) 
-				wget -O atom-amd64.deb https://atom.io/download/deb
-				sudo dpkg -i atom-amd64.deb
-				sudo apt-get install -f
-				rm atom-amd64.deb
-				;;
-    		c) 
-				sudo apt-get install -y jupyter
-				;;
-			d) 
-				brackets_loc="https://github.com/adobe/brackets/releases/download/release-1.13/Brackets.Release.1.13.64-bit.deb"
-				wget -O brackets.deb $brackets_loc
-				sudo dpkg -i brackets.deb
-				sudo apt-get install -f
-				rm brackets.deb
-				;;
-			e) 
-				#install eclipse
-				;;
+			read idech
+			case $idech in
+				a) 
+					mkdir codefolder && cd codefolder
+					curl --progress-bar -O -L "https://go.microsoft.com/fwlink/?LinkID=760868"
+					mv * code.deb
+					sudo dpkg -i code.deb
+					sudo apt-get install -f
+					cd ..
+					rm -rf codefolder
+					;;
+				b) 
+					curl --progress-bar -O atom-amd64.deb https://atom.io/download/deb
+					sudo dpkg -i atom-amd64.deb
+					sudo apt-get install -f
+					rm atom-amd64.deb
+					;;
+				c) 
+					sudo apt-get install -y jupyter
+					;;
+				d) 
+					brackets_loc="https://github.com/adobe/brackets/releases/download/release-1.13/Brackets.Release.1.13.64-bit.deb"
+					curl --progress-bar -O brackets.deb $brackets_loc
+					sudo dpkg -i brackets.deb
+					sudo apt-get install -f
+					rm brackets.deb
+					;;
+				e) 
+					eclipse_loc="http://eclipse.bluemix.net/packages/photon/data/eclipse-inst-linux64.tar.gz"
+					sudo apt install openjdk-9-jdk
+					curl --progress-bar -O eclipse.tar.gz $eclipse_loc
+					tar xzf eclipse.tar.gz
+					./eclipse-installer/eclipse-inst
+					;;
+				f) 
+					studio_loc="https://dl.google.com/dl/android/studio/ide-zips/3.1.4.0/android-studio-ide-173.4907809-linux.zip"
+					curl --progress-bar -O studio.zip $studio_loc
+					unzip studio.zip
+					mv android-studio ~/AndroidStudio
+					chmod +x ~/AndroidStudio/bin/studio.sh
+					rm studio.zip 
+					;;
 
-    		*) 
-				echo -e "${red}That doesn't exist! Try again!."
-				;;
-		esac
-		echo -e "${green}Get to coding now!"
+				*) 
+					echo -e "${red}That doesn't exist! Try again!."
+					;;
+			esac
+			echo -e "${green}Get to coding now!"
+		7) 
+			#to be done - cursors, icon sets and themes
+			;;
+		8) 
+			sudo su
+			passwd
+			exit
+			echo -e "${green}Root secured!"
+			;;
+		9) 
+			#to be done - remove snap apps and snapd
+			;;
+		10) 
+			#uninstall account plugins and account related stuff.
+			sudo apt-get purge account-plugin-aim account-plugin-facebook account-plugin-flickr account-plugin-google account-plugin-jabber account-plugin-salut account-plugin-twitter account-plugin-windows-live account-plugin-yahoo folks-common friends friends-dispatcher friends-facebook friends-twitter gnome-contacts nautilus-sendto unity-lens-friends unity-scope-gdrive unity-scope-musicstores unity-scope-zotero libfolks-eds25 libfriends0 mcp-account-manager-uoa rhythmbox-plugin-magnatune libaccount-plugin-google --qqy
 
+			#uninstall games and other bloat
+			sudo apt-get purge gnome-accessibility-themes gnome-mines gnome-orca gnome-sudoku gnomine nautilus-sendto-empathy aisleriot brltty cheese cheese-common example-content unity-plugin-scopes unity-scope-mediascanner2 -qqy
 
-	7) 
-		;;
-	8) 
-		sudo su
-		passwd
-		exit
-		echo -e "${green}Root secured!"
-		;;
-	9) 
-		;;
-	10) 
-		sudo apt install ubuntu-restricted-extras
-		echo -e "${green}Music time!"
-		;;
-	11) 
-		;;
-	12) 
-		;;
+			#uninstall zeitgeist usage tracking or whatever it does
+			sudo apt-get purge zeitgeist zeitgeist-datahub zeitgeist-core python-zeitgeist rhythmbox-plugin-zeitgeist -qqy
 
+			#uninstall rhythmbox's firefox plugin
+			sudo apt-get purge rhythmbox-mozilla -qqy
 
-	15) 
-		;;
-	*) 
-		echo "${red}Wrong choice, kiddo." ;;
-esac
+			#uninstall Ubuntu's crash stats daemon
+			sudo apt-get purge whoopsie whoopsie-preferences -qqy
+
+			#cleanup operations
+			sudo apt-get autoremove --purge -qqy
+			sudo apt-get autoclean -qqy
+			sudo apt-get clean -qqy		
+			echo -e "${green}Its squeaky clean!"
+			;;
+		11) 
+			sudo apt-get autoremove --purge -qqy
+			sudo apt-get autoclean -qqy
+			sudo apt-get clean -qqy
+			echo -e "${green}Wasn't it squeaky clean before? Well, now it is!"
+			;;
+		12) 
+			#to be done - uninstall default apps and install preferred apps
+			;;
+		13) 
+			sudo apt install -y ubuntu-restricted-extras
+			echo -e "${green}Music time!"
+			;;
+		14) 
+			#to be done - set up vs code extensions
+			;;
+
+		15) 
+			echo -e "${green}Buhbye!"
+			break
+			;;
+		*) 
+			echo "${red}Wrong choice, kiddo." ;;
+	esac
+
+done
